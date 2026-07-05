652f6623db6901a994737690236e78b192289803
Danniel <danniel.ang8@gmail.com>
2026-07-06 02:45:04 +0700

    feat(loading): adding loading orange using TimelineView

---
+++ Chal4_ADA/View/Widget/LoadOrange.swift
@@ -0,0 +1,46 @@
+//
+//  LoadOrange.swift
+//  Chal4_ADA
+//
+//  Created by Danniel on 06/07/26.
+//
+//
+
+import SwiftUI
+
+struct LoadOrange: View {
+    
+    let orangeFrames: [Image] = [
+        Image(.orangeLoad0),
+        Image(.orangeLoad1),
+        Image(.orangeLoad2),
+        Image(.orangeLoad3),
+        Image(.orangeLoad4)
+    ]
+    
+    var frameDuration: Double = 0.5
+    
+    var body: some View {
+        TimelineView(.periodic(from: .now, by: frameDuration)) { context in
+            let tick = Int(context.date.timeIntervalSince1970 / frameDuration)
+            let index = tick % orangeFrames.count
+            let dotString = String(repeating: ".", count: (index + 1) % 4)
+            
+            let _ = print("timeline tick \(context.date)")
+            
+            VStack (spacing: 16){
+                orangeFrames[index]
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 125, height: 158)
+                Text("Waiting\(dotString)")
+
+            }
+
+        }
+    }
+}
+
+#Preview {
+    LoadOrange()
+}
